b6393f79f8f692ed7c2ccaf09e58a6b21fefdf1c
Marcin Adamski <dead0user@gmail.com>
2024-04-16 22:56:43 +0200

    initial commit
    basic configuration with autopairs and toggleterm

---
+++ lua/plugins/init.lua
@@ -0,0 +1,4 @@
+return {
+  {'windwp/nvim-autopairs', event = "InsertEnter", config = true},
+  {'akinsho/toggleterm.nvim', version = "*", config = true}
+}
---
+++ lua/user/options.lua
@@ -0,0 +1,8 @@
+vim.opt.mouse = "a"
+vim.opt.undofile = true
+vim.opt.number = true
+vim.opt.fileencoding = "utf-8"
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.cursorline = true
