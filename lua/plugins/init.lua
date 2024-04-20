e5adddc5a5c00a8003beeac3de6de22b55cb8d28
Marcin Adamski <dead0user@gmail.com>
2024-04-20 23:19:29 +0200

    first plugins, new colorscheme
    Added tokyonight color scheme and Mason plugin (not configured yet)

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,4 +1,23 @@
 return {
-  {'windwp/nvim-autopairs', event = "InsertEnter", config = true},
-  {'akinsho/toggleterm.nvim', version = "*", config = true}
+  {
+    "windwp/nvim-autopairs",
+    event = "InsertEnter",
+    config = true
+  },
+  {
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    config = true}
+    ,
+  {
+    "williamboman/mason.nvim",
+    config = true
+  },
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+    enabled = true
+  }
 }
