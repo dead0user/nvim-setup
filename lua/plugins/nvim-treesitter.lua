2a41562ddd6390d7b7474d569f6fc970c0e13075
Marcin Adamski <dead0user@gmail.com>
2024-05-30 00:32:25 +0200

    Added minor changes

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,4 +1,23 @@
 return {
   "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
+  version = false,
+  build = function()
+    require("nvim-treesitter.install").update({ with_sync = true })
+  end,
+  config = function()
+    require("nvim-treesitter.configs").setup({
+      ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "python", "javascript", "rust" },
+      auto_install = false,
+      highlight = { enable = true, additional_vim_regex_highlighting = false },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<C-n>",
+          node_incremental = "<C-n>",
+          scope_incremental = "<C-s>",
+          node_decremental = "<C-m>",
+        }
+      }
+    })
+  end
 }
