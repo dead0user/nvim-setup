4acc328c9faa9e0e00123c76156c4b32669174ca
Marcin Adamski <dead0user@gmail.com>
2024-10-27 22:13:19 +0100

    minor changes in none-ls config

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -5,7 +5,15 @@ return {
     null_ls.setup({
       sources = {
         null_ls.builtins.formatting.stylua,
-        -- null_ls.builtins.diagnostics.
+        null_ls.builtins.formatting.pyink,
+        null_ls.builtins.formatting.markdownlint,
+        -- null_ls.builtins.diagnostics.eslint,
+        null_ls.builtins.completion.spell,
+        null_ls.builtins.completion.luasnip,
+        -- null_ls.builtins.diagnostics.ruff,
+        -- null_ls.builtins.formatting.black,
+        null_ls.builtins.diagnostics.markdownlint_cli2,
+        null_ls.builtins.diagnostics.pylint,
       },
     })
   end,
