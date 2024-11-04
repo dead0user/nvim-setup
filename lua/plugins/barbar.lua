03e217275379d99ef066bce593501e0fa7546770
Marcin Adamski <dead0user@gmail.com>
2024-11-04 19:35:09 +0100

    Minor config changes of barbar plugin

--- lua/plugins/barbar.lua
+++ lua/plugins/barbar.lua
@@ -5,6 +5,12 @@ return {
     'lewis6991/gitsigns.nvim',
   },
   init = function() vim.g.barbar_auto_setup = false end,
+  
+  vim.keymap.set("n", "<leader>b", "<Ignore>", { desc = "Buffers" }),
+  vim.keymap.set("n", "<leader>bp", ":BufferPrevious<CR>", { desc = "Previous buffer" }),
+  vim.keymap.set("n", "<leader>bn", ":BufferNext<CR>", { desc = "Next buffer" }),
+  vim.keymap.set("n", "<leader>bc", ":BufferClose<CR>", { desc = "Close buffer"}),
+
   config = function()
     require'barbar'.setup {
       animation = true,
