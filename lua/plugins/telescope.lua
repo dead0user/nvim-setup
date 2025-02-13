657542cc38f70748214fd3356ff92aac6712407c
Marcin Adamski <dead0user@gmail.com>
2025-02-13 22:41:56 +0100

    Minor changes

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -7,4 +7,49 @@ return {
   vim.keymap.set("n", "<leader>t", "<Ignore>", { desc = "Telescope" }),
   vim.keymap.set("n", "<leader>tf", ":Telescope find_files<cr>", { desc = "Telescope - files" }),
   vim.keymap.set("n", "<leader>tb", ":Telescope buffers<cr>", { desc = "Telescope - buffers" }),
+
+  config = function()
+    require('telescope').setup{
+      defaults = {
+        vimgrep_arguments = {
+          'rg',
+          '--color=never',
+          '--no-heading',
+          '--with-filename',
+          '--line-number',
+          '--column',
+          '--smart-case'
+        },
+        prompt_prefix = "> ",
+        selection_caret = "> ",
+        entry_prefix = " ",
+        initial_mode = "insert",
+        selection_strategy = "reset",
+        sorting_strategy = "descending",
+        layout_strategy = "horizontal",
+        layout_config = {
+          horizontal = {
+            mirror = false,
+          },
+          vertical = {
+            mirror = false,
+          },
+        },
+        file_sorter = require'telescope.sorters'.get_fuzzy_file,
+        file_ignore_patterns = {},
+        generic_sorter = require'telescope.sorters'.get_generic_fuzzy_sorter,
+        path_display = {"absolute"},
+        winblend = 0,
+        border = {},
+        borderchars = {'─', '│', '─', '│', '┌', '┐', '┘', '└'},
+        color_devicons = true,
+        use_less = true,
+        set_env = { ['COLORTERM'] = 'truecolor' }, -- default = nil,
+        file_previewer = require'telescope.previewers'.vim_buffer_cat.new,
+        grep_previewer = require'telescope.previewers'.vim_buffer_vimgrep.new,
+        qflist_previewer = require'telescope.previewers'.vim_buffer_qflist.new,
+        buffer_previewer_maker = require'telescope.previewers'.buffer_previewer_maker,
+      }
+    }
+  end;
 }
